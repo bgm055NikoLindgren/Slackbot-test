b8ce183f99dbda21d9c25ca0db0aa50a1d8d0510
Niko Lindgren <bgm055@myy.haaga-helia.fi>
2022-01-28 10:46:11 +0200

    first commit

---
+++ slack-bot/src/test/java/test/slackbot/SlackBotApplicationTests.java
@@ -0,0 +1,13 @@
+package test.slackbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
